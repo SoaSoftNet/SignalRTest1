ec882a254c607f521dd379deff1ff27815b7ff8a
xingzheng <xingzheng@outlook.com>
2020-03-10 15:22:33 +0800

    Add project files.

---
+++ SingalRClient1/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SingalRClient1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            p.Test();
+            Console.ReadKey();
+
+            
+        }
+
+        async void Test()
+        {
+            var connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:44354/NotificationHub")
+                .Build();
+
+            await connection.StartAsync();
+
+            Console.WriteLine("Starting connection. Press Ctrl-C to close.");
+            var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (sender, a) =>
+            {
+                a.Cancel = true;
+                cts.Cancel();
+            };
+
+            connection.Closed += e =>
+            {
+                Console.WriteLine("Connection closed with error: {0}", e);
+
+                cts.Cancel();
+                return Task.CompletedTask;
+            };
+
+
+            connection.On<string>("Notify", (a) =>
+            {
+                Console.WriteLine($"Notify: {a}");
+            });
+        }
+    }
+}
---
+++ SingalRTest1/Controllers/NotificationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SingalRTest1.Hubs;
+
+namespace SingalRTest1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly IHubContext<NotificationHub> _hubContext;
+
+        public NotificationController(IHubContext<NotificationHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Notify()
+        {
+            await _hubContext.Clients.All.SendAsync("Notify", $"It's time: {DateTime.Now}");
+            return Ok();
+        }
+    }
+}
