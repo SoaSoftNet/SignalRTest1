548c48a0cef5ecb796e9e8fb7016e946b3c5799f
xingzheng <xingzheng@hotmail.com>
2020-03-19 23:35:44 +0800

    add UWP project

---
+++ UwpWithSignalR/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http.Connections.Client;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UwpWithSignalR
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Connect()
+        {
+
+            Action<HttpConnectionOptions> configureHttpConnection = (opts) => {
+                opts.HttpMessageHandlerFactory = (message) =>
+                {
+                    if (message is HttpClientHandler clientHandler)
+                        // bypass SSL certificate
+                        clientHandler.ServerCertificateCustomValidationCallback +=
+                            (sender, certificate, chain, sslPolicyErrors) => { return true; };
+                    return message;
+                };
+            };
+
+            var connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:44354/NotificationHub", configureHttpConnection)
+                .Build();
+
+            await connection.StartAsync();
+
+            connection.On<string>("Notify", async (arg) =>
+            {
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => textBlock.Text = arg); 
+            });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Connect();
+        }
+    }
+}
